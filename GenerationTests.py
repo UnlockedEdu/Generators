9049f62be6523529628acd311d8c27d4970fa321
Cameron Egger <cameronegger@utexas.edu>
2019-04-08 13:54:05 -0500

    Began unittest work

--- GenerationTests.py
+++ GenerationTests.py
@@ -2,109 +2,116 @@ from unittest import main, TestCase
 import WorksheetGenerator
 
 class MyUnitTests(TestCase):
-	def test_toBaseTen_dif1();
+	def test_toBaseTen_dif1(self):
+		output = toBaseTen(1)
+		output = output.split("_")
+		assertTrue(output[0] <= 32 and output[0] >= 2)
 		pass
-	def test_toBaseTen_dif2();
+	def test_toBaseTen_dif2(self):
+		output = toBaseTen(2)
 		pass
-	def test_toBaseTen_dif3();
+	def test_toBaseTen_dif3(self):
+		output = toBaseTen(3)
 		pass
-	def test_toBaseTen_dif4();
+	def test_toBaseTen_dif4(self):
+		output = toBaseTen(4)
 		pass
-	def test_toBaseTen_dif5();
-		pass
-
-
-	def test_baseConversion_dif1():
-		pass
-	def test_baseConversion_dif2():
-		pass
-	def test_baseConversion_dif3():
-		pass
-	def test_baseConversion_dif4():
-		pass
-	def test_baseConversion_dif5():
+	def test_toBaseTen_dif5(self):
+		output = toBaseTen(5)
 		pass
 
 
-	def test_singleIfWkst_dif1():
+	def test_baseConversion_dif1(self):
 		pass
-	def test_singleIfWkst_dif2():
+	def test_baseConversion_dif2(self):
 		pass
-	def test_singleIfWkst_dif3():
+	def test_baseConversion_dif3(self):
 		pass
-	def test_singleIfWkst_dif4():
+	def test_baseConversion_dif4(self):
 		pass
-	def test_singleIfWkst_dif5():
+	def test_baseConversion_dif5(self):
 		pass
 
 
-	def test_IfElseWkst_dif1():
+	def test_singleIfWkst_dif1(self):
 		pass
-	def test_IfElseWkst_dif2():
+	def test_singleIfWkst_dif2(self):
 		pass
-	def test_IfElseWkst_dif3():
+	def test_singleIfWkst_dif3(self):
 		pass
-	def test_IfElseWkst_dif4():
+	def test_singleIfWkst_dif4(self):
+		pass
+	def test_singleIfWkst_dif5(self):
+		pass
+
+
+	def test_IfElseWkst_dif1(self):
+		pass
+	def test_IfElseWkst_dif2(self):
+		pass
+	def test_IfElseWkst_dif3(self):
+		pass
+	def test_IfElseWkst_dif4(self):
 		pass
 	def test_IfElseWkst_dif5():
 		pass
 
 
-	def test_IfElseIfElseWkst_dif1():
+	def test_IfElseIfElseWkst_dif1(self):
 		pass
-	def test_IfElseIfElseWkst_dif2():
+	def test_IfElseIfElseWkst_dif2(self):
 		pass
-	def test_IfElseIfElseWkst_dif3():
+	def test_IfElseIfElseWkst_dif3(self):
 		pass
-	def test_IfElseIfElseWkst_dif4():
+	def test_IfElseIfElseWkst_dif4(self):
 		pass
-	def test_IfElseIfElseWkst_dif5():
-		pass
-
-
-	def test_StringMethodProblems_dif1():
-		pass
-	def test_StringMethodProblems_dif2():
-		pass
-	def test_StringMethodProblems_dif3():
-		pass
-	def test_StringMethodProblems_dif4():
-		pass
-	def test_StringMethodProblems_dif5():
+	def test_IfElseIfElseWkst_dif5(self):
 		pass
 
 
-    def test_classProblems_dif1():
+	def test_StringMethodProblems_dif1(self):
+		pass
+	def test_StringMethodProblems_dif2(self):
+		pass
+	def test_StringMethodProblems_dif3(self):
+		pass
+	def test_StringMethodProblems_dif4(self):
+		pass
+	def test_StringMethodProblems_dif5(self):
+		pass
+
+
+    def test_classProblems_dif1(self):
     	pass
-    def test_classProblems_dif2():
+    def test_classProblems_dif2(self):
     	pass
-    def test_classProblems_dif3():
+    def test_classProblems_dif3(self):
     	pass
-    def test_classProblems_dif4():
+    def test_classProblems_dif4(self):
     	pass
-    def test_classProblems_dif5():
+    def test_classProblems_dif5(self):
     	pass
 
 
-    def test_arrayIteration_dif1():
+    def test_arrayIteration_dif1(self):
     	pass
-    def test_arrayIteration_dif2():
+    def test_arrayIteration_dif2(self):
     	pass
-    def test_arrayIteration_dif3():
+    def test_arrayIteration_dif3(self):
     	pass
-    def test_arrayIteration_dif4():
+    def test_arrayIteration_dif4(self):
     	pass
-    def test_arrayIteration_dif5():
+    def test_arrayIteration_dif5(self):
     	pass
 
 
-    def test_simpleRecur_dif1():
+    def test_simpleRecur_dif1(self):
     	pass
-    def test_simpleRecur_dif2():
+    def test_simpleRecur_dif2(self):
     	pass
-    def test_simpleRecur_dif3():
+    def test_simpleRecur_dif3(self):
     	pass
-    def test_simpleRecur_dif4():
+    def test_simpleRecur_dif4(self):
     	pass
-    def test_simpleRecur_dif5():
+    def test_simpleRecur_dif5(self):
     	pass
